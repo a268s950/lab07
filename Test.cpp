dc608a632cb994d65506e832286cc38638857515
Anna Seib <anna.seib@ku.edu>
2018-04-19 12:37:03 -0500

    Test addBack() fails

--- Test.cpp
+++ Test.cpp
@@ -61,3 +61,62 @@
 			return false;
 		}
 	}
+
+	/** @pre None
+	*   @post None
+	*   @return A standard vector with the contents of the list
+	*   NOTE: This method is guaranteed to work. It's only one guaranteed though
+	*/
+	//std::vector<int> toVector() const;
+
+	/** @pre the value is a valid T.
+	*   @post One new element added to the end of the list.
+	*   @return none.
+	*/
+
+	// bool Test::testfunction() {
+	// 	std::cout << "PRE" << std::endl;
+	// 	LinkedListOfInts LLo;
+	// 	int initialSize = LLo.toVector().size();
+	// 	std::vector<int> LLv;
+	// 	LLv = LLo.toVector();
+	// 	std::cout << "POST" << std::endl;
+	// 	LLv[0];
+	// 	return true;
+	// }
+
+	bool Test::TestaddBack()//FAILS
+	//add back adds to the front
+
+	{
+		LinkedListOfInts LLObject;
+		LLObject.addBack(1);
+		LLObject.addBack(2);
+		//assuming initial size is zero
+		//if(LLObject.toVector().size()==0)
+		//below line was creating seg faults so made our own vector
+		int vectorSize = LLObject.toVector().size();
+		std::vector<int> LLVector;
+		LLVector = LLObject.toVector();
+		if(LLObject.toVector().size()!=0)
+		{
+			if(LLVector[vectorSize-1] == 2)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		{
+			return false;
+		}
+
+	}
+
+	/** @pre the value is a valid T.
+	*   @post One new element added to the front of the list.
+	*   @return none.
+	*/
